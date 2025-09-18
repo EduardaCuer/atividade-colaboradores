a553de8e15335a2d795268e48b07a8272af59e76
EduardaCuer <maria.silva117@estudante.ifms.edu.br>
2025-09-18 15:10:00 -0400

    funcionalidade 2 implementada
    funcionalidade 2 implementada

---
+++ funcionalidade2.js
@@ -0,0 +1,9 @@
+// Funcionalidade 2: Dobrar um número
+function dobrar(num) 
+    return num * 2;
+}
+
+// Testando a funcionalidade
+console.log(dobrar(5));   // Deve imprimir 10
+console.log(dobrar(0));   // Deve imprimir 0
+console.log(dobrar(-3));  // Deve imprimir -6
